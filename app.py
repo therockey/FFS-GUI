7be46107a20727a7602b0f744a7e51d19df9fed5
therockey <kacperus.tomczyk@gmail.com>
2024-05-13 12:52:08 +0200

    app.py: Refactor, corrected main app window settings

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-import tkinter as tk
 from customtkinter import *
 from view.login import Login
 from viewlist import ViewType
+from prefs import preferences
+
 
 class App(CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("450x800")
         self.current_ui = []
+        self.title("FastFileStore")
+        self.resizable(False, False)
 
-        self.menu_bar = tk.Menu(self)
-        self.config(menu=self.menu_bar)
+        self.main_content = Login(self)
+        self.changeView(ViewType.LOGIN)
 
-        self.menu_bar.add_command(
-            label='Exit',
-            command=quit,
-        )
+        self.iconbitmap(preferences["ICON_PATH"])
 
-        self.menu_bar.config(bg="black", fg="black")
-
-        self.main_content = Login(self, self.change_color_theme)
-        self.main_content.pack(expand=True, fill=BOTH)
-        self.current_ui.append(self.main_content)
+        self.logged_in = False
 
     def quit(self) -> None:
         self.destroy()
@@ -31,18 +26,24 @@ class App(CTk):
         match view:
             case ViewType.LOGIN:
                 self.main_content.destroy()
-                self.main_content = Login(self, self.change_color_theme)
+                self.geometry("450x450")
+                self.main_content = Login(self)
+                self.main_content.grid(row=0, column=0, sticky="")
+                self.grid_rowconfigure(0, weight=1)
+                self.grid_columnconfigure(0, weight=1)
+                self.current_ui.append(self.main_content)
             case ViewType.MENU:
-                pass
+                print("Menu")
             case ViewType.UPLOAD:
                 pass
             case ViewType.FILE_LIST:
                 pass
 
-
+    @property
+    def login_status(self):
+        return self.logged_in
 
 
 if __name__ == "__main__":
     app = App()
-    app.title("FastFileStore")
     app.mainloop()
