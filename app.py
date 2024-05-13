4421a81e05c1f33101346733beb3642ed1ac1ec9
therockey <kacperus.tomczyk@gmail.com>
2024-05-13 21:03:12 +0200

    app.py: Added context menu window option methods, refactored changeView method

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from customtkinter import *
 from view.login import Login
+from view.menu import Menu
+from view.preferences import Preferences
+from view.about import About
 from viewlist import ViewType
 from prefs import preferences
+from SlickCTk.slick_context_menu import SlickContextMenu
 
 
 class App(CTk):
@@ -9,23 +13,36 @@ class App(CTk):
         super().__init__()
 
         self.current_ui = []
+
         self.title("FastFileStore")
+        self.iconbitmap(preferences["ICON_PATH"])
         self.resizable(False, False)
+
+        self.logged_in = False
+        self.pref_window = None
+        self.about_window = None
 
         self.main_content = Login(self)
         self.changeView(ViewType.LOGIN)
 
-        self.iconbitmap(preferences["ICON_PATH"])
+        menu_options = {
+            "Preferences": self.open_settings,
+            "About": self.open_about,
+            "Quit": self.quit,
+        }
 
-        self.logged_in = False
+        self.menu = SlickContextMenu(self, menu_options)
 
     def quit(self) -> None:
         self.destroy()
 
     def changeView(self, view: ViewType) -> None:
+        if self.main_content is not None or not self.main_content.winfo_exists():
+            self.main_content.destroy()
+            self.current_ui.clear()
+
         match view:
             case ViewType.LOGIN:
-                self.main_content.destroy()
                 self.geometry("450x450")
                 self.main_content = Login(self)
                 self.main_content.grid(row=0, column=0, sticky="")
@@ -33,7 +50,11 @@ class App(CTk):
                 self.grid_columnconfigure(0, weight=1)
                 self.current_ui.append(self.main_content)
             case ViewType.MENU:
-                print("Menu")
+                self.geometry("800x450")
+                self.main_content = Menu(self)
+                self.main_content.pack(expand=True, fill=BOTH)
+                self.current_ui.append(self.main_content)
+
             case ViewType.UPLOAD:
                 pass
             case ViewType.FILE_LIST:
@@ -42,6 +63,18 @@ class App(CTk):
     @property
     def login_status(self):
         return self.logged_in
+
+    def open_settings(self):
+        if self.pref_window is None or not self.pref_window.winfo_exists():
+            self.pref_window = Preferences(self)
+        else:
+            self.pref_window.focus()
+
+    def open_about(self):
+        if self.about_window is None or not self.about_window.winfo_exists():
+            self.about_window = About(self)
+        else:
+            self.about_window.focus()
 
 
 if __name__ == "__main__":
