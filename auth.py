62c140c2dabff55063e6a8f1e5dbb141627f3b85
therockey <kacperus.tomczyk@gmail.com>
2024-05-20 10:43:03 +0200

    auth.py: changed return type of login and register methods to also return a string representing potential error.

--- auth.py
+++ auth.py
@@ -2,15 +2,17 @@ import requests
 from prefs import preferences
 
 
-def login(username: str, password: str) -> bool:
+def login(username: str, password: str) -> (bool, str):
     print(username, password)
     response = requests.post(f"{preferences['API_URL']}/login/", data={"username": username, "password": password})
-    return True if response.status_code == 200 and response.json()['status'] == "success" else False
+    return (True, "success") if response.status_code == 200 and response.json()['status'] == "success" else (
+        False, response.json()['message'])
 
 
-def register(username: str, password: str) -> bool:
+def register(username: str, password: str) -> (bool, str):
     response = requests.post(f"{preferences['API_URL']}/register/", data={"username": username, "password": password})
-    return True if response.status_code == 200 and response.json()['status'] == "success" else False
+    return (True, "success") if response.status_code == 200 and response.json()['status'] == "success" else (
+        False, response.json()['message'])
 
 
 def get_token() -> str:
