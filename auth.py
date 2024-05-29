0a799fe1583bb10eedb356041d906b3c365d5286
therockey <kacperus.tomczyk@gmail.com>
2024-05-29 23:24:55 +0200

    auth.py: changed method return so that login now yields the token in case of successful login

--- auth.py
+++ auth.py
@@ -1,19 +1,30 @@
+from datetime import datetime
 import requests
 from prefs import preferences
 
 
-def login(username: str, password: str) -> (bool, str):
+def login(username: str, password: str) -> (bool, str, datetime):
     response = requests.post(f"{preferences['API_URL']}/login/", data={"username": username, "password": password})
-    return (True, "success") if response.status_code == 200 and response.json()['status'] == "success" else (
-        False, response.json()['message'])
+
+    status = response.json()['status']
+    code = response.status_code
+
+    print(f"{response.json()['session_expiry']}")
+
+    if status == "success" and code == 200:
+        token = response.json().get('session_key', None)
+        expiry = datetime.strptime(response.json()['session_expiry'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        return True, token, expiry
+
+    return False, None, None
 
 
 def register(username: str, password: str) -> (bool, str):
     response = requests.post(f"{preferences['API_URL']}/register/", data={"username": username, "password": password})
-    return (True, "success") if response.status_code == 200 and response.json()['status'] == "success" else (
-        False, response.json()['message'])
 
+    status = response.json()['status']
 
-def get_token() -> str:
-    response = requests.get(f"{preferences['API_URL']}/getcsrf/")
-    return response.json()["csrf_token"] if response.status_code == 200 else None
+    if status == "success":
+        return True, None
+
+    return False, response.json()['message']
